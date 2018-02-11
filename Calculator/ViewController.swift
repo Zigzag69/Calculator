989e63ed69f13dafc9778a0dc3f45feb6a26c6e1
Ivan Heveliev <zigzag69@ukr.net>
2018-02-12 01:03:34 +0200

    Add some functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,6 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var firstLabel: UILabel!
+    var stillTyping = false
+    var x: Double = 0
+    var y: Double = 0
+    var operationSign: String = ""
+    
+    var currentInput: Double  {
+        get {
+            return Double(firstLabel.text!)!
+        }
+        set {
+            firstLabel.text	= "\(newValue)"
+            stillTyping = false
+        }
+    }
     
     @IBOutlet weak var secondLabel: UILabel!
     
@@ -20,11 +34,47 @@ class ViewController: UIViewController {
 
     @IBAction func numbersBut(_ sender: UIButton) {
         let numbers = sender.currentTitle!
+        
+        if stillTyping {
+            if (firstLabel.text?.count)! < 20 {
         firstLabel.text = firstLabel.text! + numbers
+        }
+      }
+        else {
+            firstLabel.text = numbers
+            stillTyping = true
+        }
     }
     
     
+    @IBAction func operations(_ sender: UIButton) {
+        operationSign = sender.currentTitle!
+        x = currentInput
+        stillTyping = false
+        }
     
+    func operateWithTwoOperands (operation: (Double, Double) -> Double) {
+        currentInput = operation (x, y)
+        stillTyping = false
+    }
+    
+    @IBAction func result(_ sender: UIButton) {
+        if stillTyping {
+            y = currentInput
+        }
+        
+        switch operationSign {
+            case "+":
+                operateWithTwoOperands{$0 + $1}
+            case "-":
+                operateWithTwoOperands{$0 - $1}
+            case "✕":
+                operateWithTwoOperands{$0 + $1}
+            case "∕":
+                operateWithTwoOperands{$0 + $1}
+        default: break
+        }
+    }
     
     
 
